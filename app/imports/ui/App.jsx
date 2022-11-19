0a4548f9a2cbed8d6622d735e5efdddea4670f74
fibek <igorfib6@gmail.com>
2022-11-19 01:20:38 +0100

    Add login form

--- app/imports/ui/App.jsx
+++ app/imports/ui/App.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Snow from './Snow.jsx';
 import LoginForm from './LoginForm.jsx';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-export const App = () => (
-  <ChakraProvider>
-    <Snow />
-    <LoginForm />
-  </ChakraProvider>
-);
+export const App = () => {
+  const user = useTracker(() => Meteor.user());
+
+  return (
+    <ChakraProvider>
+      <Snow />
+      {!user ? <LoginForm /> : <LoginForm user={user} />}
+    </ChakraProvider>
+  )
+};
--- app/imports/ui/LoginForm.jsx
+++ app/imports/ui/LoginForm.jsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Meteor } from 'meteor/meteor';
+import React, {useState} from 'react'
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Link
+} from "@chakra-ui/react";
+import {
+  Link as ReactRouterLink
+} from "react-router-dom";
 
 export default LoginForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      // Meteor.loginWithPassword(values.username,values.password);
+    }
+  });
+
   return( 
-    <Box bg='tomato' w='100%' p={4} color='white' align='center' w='100px' display='flex'>
-      This is the Box
-    </Box>
+    <Flex align="center" justify="center" h="100vh">
+      <Box bg="white" p={6} rounded="md">
+        <form onSubmit={formik.handleSubmit}>
+          <VStack spacing={4} align="flex-start">
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                variant="filled"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Hasło</FormLabel>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                variant="filled"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+              />
+            </FormControl>
+            <Button type="submit" colorScheme="purple" width="full">
+              Zaloguj
+            </Button>
+	
+	    <Link>
+	      Nie masz jeszcze konta? Zarejestruj się tutaj.
+	    </Link>
+          </VStack>
+        </form>
+      </Box>
+    </Flex>
   );
 };
