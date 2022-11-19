20c6dc6c14ecda0e257bdc1fa9a320336948032f
fibek <igorfib6@gmail.com>
2022-11-20 00:55:39 +0100

    Fix strange input behaviour and login form

--- app/imports/ui/LoginForm.jsx
+++ app/imports/ui/LoginForm.jsx
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import React, {useState} from 'react'
-import { useFormik } from "formik";
+import { Formik, Field, useFormik } from "formik";
 import {
   Box,
   Button,
@@ -8,117 +8,115 @@ import {
   Flex,
   FormControl,
   FormLabel,
+  FormErrorMessage,
   Input,
-  VStack,
-  Link
+  VStack
 } from "@chakra-ui/react";
 import { Accounts } from 'meteor/accounts-base';
 
 export default LoginForm = () => {
   const [isregister, setisregister] = useState(false);
 
-  const loginformik = useFormik({
-    initialValues: {
-      username: "",
-      password: ""
-    },
-    onSubmit: (values) => {
-      // alert(JSON.stringify(values, null, 2));
-      Meteor.loginWithPassword(values.username,values.password);
-    }
-  });
-
-  const registerformik = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-      password2:""
-    },
-    onSubmit: (values) => {
-      // alert(JSON.stringify(values, null, 2));
-      Accounts.createUser({username: values.username,password: values.password}); 
-    }
-  });
-
-  const Login = () => (
+  const Register = () => (
     <>
-      <form onSubmit={loginformik.handleSubmit}>
-        <VStack spacing={4} align="flex-start">
-	  <FormControl>
-      	    <FormLabel htmlFor="text">Nazwa użytkownika</FormLabel>
-      	    <Input
-      	      id="username"
-      	      name="username"
-      	      type="text"
-      	      variant="filled"
-      	      onChange={registerformik.handleChange}
-      	      value={loginformik.values.email}
-      	    />
-      	  </FormControl>
-      	  <FormControl>
-      	    <FormLabel htmlFor="password">Hasło</FormLabel>
-      	    <Input
-      	      id="password"
-      	      name="password"
-      	      type="password"
-      	      variant="filled"
-      	      onChange={registerformik.handleChange}
-      	      value={loginformik.values.password}
-      	    />
-      	  </FormControl>
-      	  <Button type="submit" colorScheme="purple" width="full">
-      	    Zaloguj
-      	  </Button>
-      	  <Button onClick={() => setisregister(true)}>
-      	    Nie masz jeszcze konta? Zarejestruj się tutaj.
-      	  </Button>
-        </VStack>
-      </form>
+      <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+          Accounts.createUser({username: values.username, password: values.password});
+        }}
+      >
+        {({ handleSubmit, errors, touched }) => (
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4} align="flex-start">
+              <FormControl>
+                <FormLabel>Nazwa użytkownika</FormLabel>
+                <Field
+                  as={Input}
+                  id="username"
+                  name="username"
+                  type="text"
+                  variant="filled"
+                />
+              </FormControl>
+              <FormControl isInvalid={!!errors.password && touched.password}>
+                <FormLabel htmlFor="password">Hasło</FormLabel>
+                <Field
+                  as={Input}
+                  id="password"
+                  name="password"
+                  type="password"
+                  variant="filled"
+                  validate={(value) => {
+                    let error;
+
+                    if (value.length < 5) {
+                      error = "Hasło musi zawierać conajmniej 5 znaków";
+                    }
+
+                    return error;
+                  }}
+                />
+                <FormErrorMessage>{errors.password}</FormErrorMessage>
+              </FormControl>
+              <Button type="submit" colorScheme="purple" width="full" 
+		      onClick={() => setisregister(false)}>
+                Zarejestruj
+              </Button>
+            </VStack>
+          </form>
+        )}
+      </Formik>
     </>
   );
   
-  const Register = () => (
+  const Login = () => (
     <>
-      <form onSubmit={registerformik.handleSubmit}>
-        <VStack spacing={4} align="flex-start">
-	  <FormControl>
-      	    <FormLabel htmlFor="email">Email</FormLabel>
-      	    <Input
-      	      id="email"
-      	      name="email"
-      	      type="email"
-      	      variant="filled"
-      	      onChange={registerformik.handleChange}
-      	      value={registerformik.values.email}
-      	    />
-      	  </FormControl>
-      	  <FormControl>
-      	    <FormLabel htmlFor="password">Hasło</FormLabel>
-      	    <Input
-      	      id="password"
-      	      name="password"
-      	      type="password"
-      	      variant="filled"
-      	      onChange={registerformik.handleChange}
-      	      value={registerformik.values.password}
-      	    />
-      	  </FormControl>
-      	  <FormControl>
-      	    <FormLabel htmlFor="password">Hasło</FormLabel>
-      	    <Input
-      	      id="password2"
-      	      name="password2"
-      	      type="password"
-      	      variant="filled"
-      	      onChange={registerformik.handleChange}
-      	      value={registerformik.values.password2}
-      	    />
-      	  </FormControl>
-      	  <Button type="submit" colorScheme="purple" width="full" onClick={() => setisregister(false)}>
-      	    Zarejestruj
-      	  </Button>
-        </VStack>
-      </form>
+       <Formik
+        initialValues={{
+          username: "",
+          password: "",
+        }}
+        onSubmit={(values) => {
+	  Meteor.loginWithPassword(values.username,values.password);
+        }}
+      >
+        {({ handleSubmit, errors, touched }) => (
+          <form onSubmit={handleSubmit}>
+            <VStack spacing={4} align="flex-start">
+              <FormControl>
+                <FormLabel>Nazwa użytkownika</FormLabel>
+                <Field
+                  as={Input}
+                  id="username"
+                  name="username"
+                  type="text"
+                  variant="filled"
+                />
+              </FormControl>
+              <FormControl isInvalid={!!errors.password && touched.password}>
+                <FormLabel htmlFor="password">Hasło</FormLabel>
+                <Field
+                  as={Input}
+                  id="password"
+                  name="password"
+                  type="password"
+                  variant="filled"
+                />
+                <FormErrorMessage>{errors.password}</FormErrorMessage>
+              </FormControl>
+              <Button type="submit" colorScheme="purple" width="full">
+                Zaloguj
+              </Button>
+              <Button onClick={() => setisregister(true) }>
+		Nie masz jeszcze konta? Zarejestruj się tutaj! 
+              </Button>
+            </VStack>
+          </form>
+        )}
+      </Formik>
     </>
   );
 
