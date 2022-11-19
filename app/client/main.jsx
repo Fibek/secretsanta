442db2d3b06fc79ea7461683b2fca8a8621fa15e
fibek <igorfib6@gmail.com>
2022-11-19 01:20:00 +0100

    init

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -3,10 +3,31 @@ import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { App } from '/imports/ui/App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container); // createRoot(container!)
 
-  root.render(<App/>);
+  root.render(<App />);
+  // const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <App />
+  // },
+  // {
+  //   path: "/register",
+  //   element: <RegisterForm />
+  // },
+// ]);
+
+  // root.render(
+  //   <React.StrictMode>
+  //     <RouterProvider router={router} />
+  //   </React.StrictMode>
+  // );
 });
