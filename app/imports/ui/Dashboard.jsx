d7aa795491569f42e2158f5dfcd698a8754b0cac
fibek <igorfib6@gmail.com>
2022-11-24 22:38:35 +0100

    Add simple dashboard

--- app/imports/ui/Dashboard.jsx
+++ app/imports/ui/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import React, {useState} from 'react'
 import { useFormik } from "formik";
 import {
@@ -11,15 +12,52 @@ import {
   Input,
   VStack,
   Link,
-  Text
+  Heading,
+  Spacer,
+  Icon,
+  Textarea,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Lorem
 } from "@chakra-ui/react";
+import { BiUserCircle } from 'react-icons/bi'
 
 export default Dashboard = () => {
+  const user = useTracker(() => Meteor.user());
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return( 
     <Flex align="center" justify="center" h="100vh">
-      <Box bg="white" p={6} rounded="md">
-	<Text> Dashboard </Text>
+      <Box bg="white" p={6} rounded="md" w={800}>
+	<Flex>
+	  <Heading> Witaj, { user.username }!  </Heading>
+	  <Spacer />
+	  <Button variant="ghost" onClick={onOpen}> 
+	    <Icon as={BiUserCircle} w={12} h={12} />
+	  </Button>
+	  <Modal isOpen={isOpen} onClose={onClose} isCentered>
+	    <ModalOverlay />
+	    <ModalContent>
+	      <ModalHeader>Modal Title</ModalHeader>
+	      <ModalCloseButton />
+	      <ModalFooter>
+		<Button colorScheme='blue' mr={3} onClick={onClose}>
+            	  Close
+            	</Button>
+            	<Button variant='ghost'>Secondary Action</Button>
+	      </ModalFooter>
+	    </ModalContent>
+	  </Modal>
+        </Flex>
+
+	<Textarea placeholder="Jesli chcesz, to wpisz tutaj wskazowki." />
+	<Button> Zapisz </Button>
       </Box>
     </Flex>
   );
